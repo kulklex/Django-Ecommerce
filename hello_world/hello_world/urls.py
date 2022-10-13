81bc6d02e5c8d76638454ab7481529e94b2ffa32
kulklex <kulklex@gmail.com>
2022-10-13 14:39:22 +0100

    Implementation of  ListView

--- hello_world/hello_world/urls.py
+++ hello_world/hello_world/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
-    path('crud/', include('CRUD.urls'))
+    path('crud/', include('CRUD.urls')),
+    path('posts/', include('posts.urls'))
 ]
--- hello_world/posts/views.py
+++ hello_world/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView
 
 # Create your views here.
+class PostPageView(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'all_posts_list'
