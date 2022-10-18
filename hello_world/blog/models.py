69030e0ef0f25790b6464e2d57518bfb152d3a42
kulklex <kulklex@gmail.com>
2022-10-18 12:25:57 +0100

    created BlogPost model

--- hello_world/blog/models.py
+++ hello_world/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=25)
+    body = models.TextField()
+    author = models.ForeignKey( 'auth.User', on_delete=models.CASCADE, default="user")
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog_post_detail", kwargs={"pk": self.pk})
+    
