81ddd4e4a19407b63ffda5f23b25a96d4f941872
kulklex <kulklex@gmail.com>
2022-10-22 00:19:25 +0100

    fixed queryset bug

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -6,18 +6,18 @@ from .serializers import RealtorSerializer
 # Create your views here.
 class RealtorListView(ListAPIView):
     permission_classes = (permissions.AllowAny, )   #Allows any one to access the view 
-    Queryset = Realtor.objects.all()
+    queryset = Realtor.objects.all()
     serializer_class = RealtorSerializer
     pagination_class = None
 
 
 class RealtorView(RetrieveAPIView):
-    Queryset = Realtor.objects.all()
+    queryset = Realtor.objects.all()
     serializer_class = RealtorSerializer
     
     
 class TopSellerView(ListAPIView):
     permission_classes = (permissions.AllowAny, )   #Allows any one to access the view 
-    Queryset = Realtor.objects.filter(top_seller=True)
+    queryset = Realtor.objects.filter(top_seller=True)
     serializer_class = RealtorSerializer
     pagination_class = None
