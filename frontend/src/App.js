2a23ed4e395e67edde994f3dd8116eb6e8c135f8
kulklex <kulklex@gmail.com>
2023-01-07 03:23:45 +0100

    Integrating Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Listings from './pages/Listings';
+import ListingDetail from './pages/ListingDetail';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Page404 from './components/Page404/Page404';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter className="App">
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/listings' element={<Listings/>} />
+          <Route path='/listings/:id' element={<ListingDetail/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/login' element={<Login/>} />
+          
+          <Route path='*' element={<Page404/>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
