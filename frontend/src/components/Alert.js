e95867239a98b6a5e56db15579864025bd610cdb
kulklex <kulklex@gmail.com>
2023-01-30 16:14:48 +0100

    Alert and Navbar components

---
+++ frontend/src/components/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = ({alerts}) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
+<div key={alert?.id} className=''>
+    {alert?.msg}
+</div>));
+
+Alert.prototype = {
+    alert: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+    alerts: state.alert
+})
+
+export default connect(mapStateToProps)(Alert);
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Connect } from 'react-redux';
+import Alert from './Alert';
+import PropTypes from 'prop-types'
 
 const Navbar = () => {
     return (
