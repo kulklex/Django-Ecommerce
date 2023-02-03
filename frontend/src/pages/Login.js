584ab9a7f0f71c94a37e1735c4ac15b63824ccd9
kulklex <kulklex@gmail.com>
2023-02-03 10:06:41 +0100

    Sign Up and Log In works!

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Helmet} from 'react-helmet'
+import { login } from '../redux/actions/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const {email, password} = formData
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(login({email, password}, navigate))
+    }
+
+
     return (
-        <div>
-            
+        <div className='auth'>
+            <Helmet>
+                <title>Real-Estate Login</title>
+                <meta name='description' content='login page'/>
+            </Helmet>
+            <div className="auth__title">Sign In</div>
+            <div className="auth__lead">Sign into your Account</div>
+            <form className='auth__form' onSubmit={(e) => handleSubmit(e)}>
+                <div className="auth__form__group">
+                    <input type="email" name='email' placeholder='Email' value={email} className="auth__form__input" required onChange={e => handleChange(e)}/>
+                </div>
+                <div className="auth__form__group">
+                    <input type="password" name='password' placeholder='Password' value={password} className="auth__form__input" required onChange={e => handleChange(e)} minLength={6}/>
+                </div>
+                <button className="auth__form__button">Login</button>
+            </form>
+            <div className="auth__authtext">
+                Don't have an account? <Link className='auth__authtext_link' to='/signup'>Sign Up</Link>
+            </div>
         </div>
     );
 }
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { signup } from '../redux/actions/auth';
+import {  Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    const [formData, setFormData] = useState({
+        name: '', email: '', password: '', password2: ''
+    })
+
+    const {name, email, password, password2} = formData
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(signup({name, email, password, password2}, navigate))
+    }
+
+
     return (
-        <div>
-            
+        <div className='auth'>
+            <Helmet>
+                <title>Real-Estate Login</title>
+                <meta name='description' content='signup page'/>
+            </Helmet>
+        <div className="auth__title">Sign Up</div>
+        <div className="auth__lead">Create your Account</div>
+        <form className='auth__form' onSubmit={(e) => handleSubmit(e)}>
+            <div className="auth__form__group">
+                <input type="text" name='name' placeholder='Name' value={name} className="auth__form__input" required onChange={e => handleChange(e)} minLength={6}/>
+            </div>
+            <div className="auth__form__group">
+                <input type="email" name='email' placeholder='Email' value={email} className="auth__form__input" required onChange={e => handleChange(e)}/>
+            </div>
+            <div className="auth__form__group">
+                <input type="password" name='password' placeholder='Password' value={password} className="auth__form__input" required onChange={e => handleChange(e)} minLength={6}/>
+            </div>
+            <div className="auth__form__group">
+                <input type="password" name='password2' placeholder='Confirm Password' value={password2} className="auth__form__input" required onChange={e => handleChange(e)} minLength={6}/>
+            </div>
+            <button className="auth__form__button">Register</button>
+        </form>
+        <div className="auth__authtext">
+            Already have an account? <Link className='auth__authtext_link' to='/login'>Sign In</Link>
         </div>
+    </div>
     );
 }
 
