a89a649a7a388131b150a51c812777c0689b3867
kulklex <kulklex@gmail.com>
2023-02-09 15:39:13 +0100

    Pagination Component

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({listingPerPage, active, count, visitPage, previousPage, nextPage}) => {
+    const getNumbers = () => {
+        let numbers = []
+        let itemsPerPage = listingPerPage
+        let pageNumber = 1;
+
+        for (let i = 0; i < count; i < itemsPerPage) {
+            const page = pageNumber
+            let content = ' ';
+
+            if(active === page) {
+                content = (<div key={i} className='pagination__number pagination__number--active'>
+                    {pageNumber}
+                </div>)
+            } else {
+                content = (<div key={i} onClick={() => visitPage(page)} className='pagination__number' >
+                    {pageNumber={}}
+                </div>)
+            }
+
+            numbers.push(content)
+            pageNumber++
+        }
+        return numbers
+    }   
+
     return (
-        <div>
-            
+        <div className='pagination'>
+            <div className='pagination__number' onClick={previousPage}>Previous</div>
+            {getNumbers()}
+            <div className='pagination__number' onClick={nextPage}>Next</div>
         </div>
     );
 }
