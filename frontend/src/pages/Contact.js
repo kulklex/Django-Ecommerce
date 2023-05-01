f093b5c66817c92729d4019226f9f25fb0700c56
kulklex <kulklex@gmail.com>
2023-05-01 22:12:37 +0100

    Contact Page Impl

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import { setAlert } from '../redux/actions/alert'
 
-const Contact = () => {
+
+const Contact = ({ setAlert }) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+
+    const { name, email, subject, message } = formData;
+
+    const [loading, setLoading] = useState(false);
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        setLoading(true);
+        axios.post(`${process.env.REACT_APP_API_URL}/contacts/`, { name, email, subject, message }, config)
+        .then(res => {
+            setAlert('Message Sent', 'success');
+            setLoading(false);
+            window.scrollTo(0, 0);
+        })
+        .catch(err => {
+            setAlert('Error with Sending Message', 'error');
+            setLoading(false);
+            window.scrollTo(0, 0);
+        })
+    };
+
     return (
-        <div>
-            Contact
+        <div className='contact'>
+            <form className='contact__form' onSubmit={e => onSubmit(e)}>
+                <label className='contact__form__label' htmlFor='name'>Name*</label>
+                <input 
+                    className='contact__form__input' 
+                    name='name' 
+                    type='text' 
+                    placeholder='Full Name' 
+                    onChange={e => onChange(e)} 
+                    value={name} 
+                    required 
+                />
+                <label className='contact__form__label' htmlFor='email'>Email*</label>
+                <input 
+                    className='contact__form__input' 
+                    name='email' 
+                    type='email' 
+                    placeholder='example@gmail.com' 
+                    onChange={e => onChange(e)} 
+                    value={email} 
+                    required 
+                />
+                <label className='contact__form__label' htmlFor='subject'>Subject*</label>
+                <input 
+                    className='contact__form__input' 
+                    name='subject' 
+                    type='text' 
+                    placeholder='Buying Home' 
+                    onChange={e => onChange(e)} 
+                    value={subject} 
+                    required 
+                />
+                <label className='contact__form__label' htmlFor='message'>Message</label>
+                <textarea 
+                    className='contact__form__textarea'
+                    name='message'
+                    cols='30'
+                    rows='10'
+                    placeholder='Message'
+                    onChange={e => onChange(e)} 
+                    value={message} 
+                />
+                {loading ?
+                    <div className='contact__form__loader'>
+                        Loading....
+                    </div> :
+                    <button className='contact__form__button' type='submit'>Send</button>
+                }
+            </form>
         </div>
     );
-}
+};
 
-export default Contact;
+
+export default connect(null, { setAlert })(Contact);
